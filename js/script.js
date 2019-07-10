804fce06a413816996c6755648469f07a015cc7a
Flavio Luche <flavioluche@gmail.com>
2019-07-10 19:56:27 -0300

    Practicing now with switch case statements

--- js/script.js
+++ js/script.js
@@ -45,19 +45,20 @@ mikeGameTwo = 94;
 mikeGameThree = 123;
 
 
-//forcing a draw
+/* //forcing a draw
 
 mikeGameOne = 89;
 
 mikeGameTwo = 120;
 
 mikeGameThree = 200; //first test 103
-
+ */
 teamJohnAvg = (johnGameOne + johnGameTwo + johnGameThree) / 3;
 
 teamMikeAvg = (mikeGameOne + mikeGameTwo + mikeGameThree) / 3;
 
 /* //Using if else
+
 if (teamJohnAvg > teamMikeAvg){
     console.log('John\'s Team wins with an win average of ' + teamJohnAvg + ' over ' + teamMikeAvg +  ' of Mike\'s team ');
 } else if (teamMikeAvg > teamJohnAvg) {
@@ -65,6 +66,8 @@ if (teamJohnAvg > teamMikeAvg){
 } else {
     console.log('Mike\'s Team and John\'s team are with the same wins average of ' + teamMikeAvg);
 } */
+
+
 
 //using switch case
 
